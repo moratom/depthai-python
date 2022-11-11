7cf82b72d904e0d8bd55bada397e6237a09e65c7
moratom <47612463+moratom@users.noreply.github.com>
2022-11-11 01:42:21 +0100

    Update install_dependencies.sh

--- docs/source/_static/install_dependencies.sh
+++ docs/source/_static/install_dependencies.sh
@@ -172,7 +172,7 @@ elif [ -f /etc/os-release ]; then
     # Allow all users to read and write to Myriad X devices
     echo "Installing udev rules..."
     echo 'SUBSYSTEM=="usb", ATTRS{idVendor}=="03e7", MODE="0666"' | sudo tee /etc/udev/rules.d/80-movidius.rules > /dev/null
-    sudo udevadm control --reload-rules && sudo udevadm trigger
+    # sudo udevadm control --reload-rules && sudo udevadm trigger
 else
     echo "ERROR: Host not supported"
     exit 99
